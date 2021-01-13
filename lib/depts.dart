8c4d04f0ed6bc131bea24e7750708187f587aeb2
Goombugi <goombugi92@hotmail.com>
2021-01-13 11:22:09 +0300

    Creating the DAO (Data Access Object)

--- lib/depts.dart
+++ lib/depts.dart
@@ -9,3 +9,15 @@ class Dept{
   String description;
  Dept(this.deptType, this.amount, this.name, this.description, this.id);
 }
+
+@dao
+abstract class DeptDao{
+@Query('SELECT * FROM Dept')
+  Future<List<Dept>> findAllDept();
+
+@Query('SELECT * FROM Dept WHERE id = :id')
+  Stream<Dept> FindDeptById(int id);
+
+@insert
+  Future<void> insertDept(Dept dept);
+}
